c584269f4d0751515cfede162f5b758ed0524596
Micael Santos <micaelmaicon@hotmail.com>
2021-05-19 20:01:08 -0300

    Finalizo topico 1 do execicio

---
+++ exercises/Fundamentos/Bloco-04-Intro-JS-Lógica-De-Programação/dia-1-IntroJS-FirstSteps/proposed-execise/Topic-1-exercise-3.js
@@ -0,0 +1,14 @@
+// como pedido no tópico 1: Programa - Operador Aritimético - MULTIPLICAÇÃO - DIVISÃO - MÓDULO
+
+let a = 10;
+let b = 2;
+let multply = a * b;
+console.log("O produto entre " +a+ " e " +b+ " é: " +multply);
+
+
+let divisor = a / b;
+console.log("A razão entre " +a+ " e " +b+ " é: " +divisor);
+
+
+let mod = a % b;
+console.log("O módulo entre " +a+ " e " +b+ " é: " +mod);

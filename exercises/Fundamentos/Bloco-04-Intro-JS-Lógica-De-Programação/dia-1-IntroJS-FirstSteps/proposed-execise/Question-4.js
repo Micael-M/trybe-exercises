e5ee128a504fcc0d18c1c1b972e293d0c4b324fa
Micael Santos <micaelmaicon@hotmail.com>
2021-05-19 23:49:22 -0300

    Finish question 4

---
+++ exercises/Fundamentos/Bloco-04-Intro-JS-Lógica-De-Programação/dia-1-IntroJS-FirstSteps/proposed-execise/Question-4.js
@@ -0,0 +1,10 @@
+let arr = [1,1,-2,0];
+
+
+for(let i = 0; i < arr.length; i++){
+  if(arr[i] > 0){
+    console.log("O valor é positivo!");}
+  else if(arr[i] < 0){
+    console.log("O valor é negativo!");}
+  else{
+    console.log("Zero!");}}

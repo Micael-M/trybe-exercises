89a354e2da2443c00e83d0376b6b75f5c9a4368e
Micael Santos <micaelmaicon@hotmail.com>
2021-05-19 22:44:15 -0300

    Finish question 2

---
+++ exercises/Fundamentos/Bloco-04-Intro-JS-Lógica-De-Programação/dia-1-IntroJS-FirstSteps/proposed-execise/Question-2.js
@@ -0,0 +1,10 @@
+let a = 0;
+let b = 0;
+
+if(a > b){console.log("O maior valor entre " +a+ " e " +b+ " é: " +a);}
+
+else if(a < b){console.log("O maior valor entre " +a+ " e " +b+ " é: " +b);}
+
+else{
+    console.log("Ambos os valores são iguais!")
+}

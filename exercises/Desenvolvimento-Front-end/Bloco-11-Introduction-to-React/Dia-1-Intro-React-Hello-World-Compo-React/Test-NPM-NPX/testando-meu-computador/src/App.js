904918e48b1e7c77c7a9a19a6b4c6184a261ab06
Micael Santos <micaelmaicon@hotmail.com>
2021-07-25 15:59:13 -0300

    exercicio flexbox

---
+++ exercises/Desenvolvimento-Front-end/Bloco-11-Introduction-to-React/Dia-1-Intro-React-Hello-World-Compo-React/Test-NPM-NPX/testando-meu-computador/src/App.js
@@ -0,0 +1,10 @@
+// Tópico Classes e React - 01: 'Hello, world!' no React!
+import React from 'react';
+// import './App.css'; Por enquanto não vai precisar 
+import HelloWorld from './helloWorld';
+
+function App() {
+  return <HelloWorld /> // Referenciando a classe criada no arquivo helloword.js
+};
+
+export default App;

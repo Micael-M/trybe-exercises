5da8ca8e41c9e56f07370c281d2494ce45ffd77a
Micael Santos <micaelmaicon@hotmail.com>
2022-03-11 19:18:02 -0300

    Exercises 1 and 2

---
+++ exercises/back-end/Bloco26-TypeScript/proposed-exercise-26.2/weatherSeasons.ts
@@ -0,0 +1,9 @@
+// Exercício 1 : Crie uma enum que represente as estações climáticas do ano.
+
+enum WeatherSeasons {
+  OUTONO = "Outono",
+    INVERNO = "Inverno",
+    PRIMAVERA = "Primavera",
+    VERAO = "Verão",
+}
+export default WeatherSeasons;
